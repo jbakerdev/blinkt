655ea20745379af0bceeb16f4d5d97eb449b3338
Justin Baker <me@jbaker.dev>
2022-06-30 17:35:31 -0700

    Display color based on load

--- cpu_load.py
+++ cpu_load.py
@@ -12,15 +12,27 @@ import blinkt
 
 blinkt.set_clear_on_exit()
 
-
-def show_graph(v, r, g, b):
+def show_graph(v):
     v *= blinkt.NUM_PIXELS
+
+    spectrum = [
+        [128,   0, 128],
+        [48,   0,  192],
+        [0,     0, 255],
+        [0,   128, 128],
+        [0,   255,   0],
+        [255, 192,   0],
+        [255,  48,   0],
+        [255,   0,   0],
+    ]
+
     for x in range(blinkt.NUM_PIXELS):
         if v < 0:
             r, g, b = 0, 0, 0
         else:
-            r, g, b = [int(min(v, 1.0) * c) for c in [r, g, b]]
-        blinkt.set_pixel(x, r, g, b)
+            r, g, b = [int(min(v, 1.0) * c) for c in spectrum[x]]
+        pixel = blinkt.NUM_PIXELS - 1 - x
+        blinkt.set_pixel(pixel, r, g, b)
         v -= 1
 
     blinkt.show()
@@ -38,7 +50,7 @@ signal.signal(signal.SIGTERM, clear_leds)
 try:
     while True:
         v = psutil.cpu_percent() / 100.0
-        show_graph(v, 255, 255, 255)
+        show_graph(v)
         time.sleep(0.01)
 except KeyboardInterrupt:
     pass
